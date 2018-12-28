06249eea5d858671b81ceaacecc86d7e2cfa40dc
序歌 <jixu.hjx@antfin.com>
2018-12-28 17:58:02 +0800

    upgrade

--- app/src/main/java/proxy/ian4hu/github/com/NoneProxyFilter.kt
+++ app/src/main/java/proxy/ian4hu/github/com/NoneProxyFilter.kt
@@ -3,24 +3,27 @@ package proxy.ian4hu.github.com
 import android.util.LruCache
 import java.net.InetAddress
 import java.net.NetworkInterface
+import java.net.UnknownHostException
 import javax.servlet.*
 
 private val ATTR_NONE_PROXY_APPLIED = NoneProxyFilter::class.java.name + ".applied";
 
 class NoneProxyFilter : Filter {
 
-    private val localhostNameCache: LruCache<String, Boolean> = LruCache(512)
+    private val localHostNameCache: LruCache<String, Boolean> = LruCache(512)
+
+    private lateinit var localHostNames: Set<String>
 
     private var resolve = false
 
     override fun destroy() {
-        localhostNameCache.evictAll()
+        localHostNameCache.evictAll()
     }
 
     override fun doFilter(request: ServletRequest, response: ServletResponse, chain: FilterChain) {
         val targetHost = request.serverName
         if (
-                // The request must from outside
+        // The request must from outside
                 (DispatcherType.REQUEST.equals(request.dispatcherType) || DispatcherType.ASYNC.equals(request.dispatcherType))
                 // Only filter once
                 && true != request.getAttribute(ATTR_NONE_PROXY_APPLIED)
@@ -38,26 +41,34 @@ class NoneProxyFilter : Filter {
         filterConfig?.run {
             resolve = "true".equals(getInitParameter("resolve"), true)
         }
-        val localhostAddress = NetworkInterface.getNetworkInterfaces().toList()
+        localHostNames = NetworkInterface.getNetworkInterfaces().toList()
                 .flatMap { it.inetAddresses.toList() }
                 .map { it.hostAddress.substringBefore('%') }
-                .toSet()
-        localhostAddress.forEach {
-            localhostNameCache.put(it, true)
+                .toSet() + InetAddress.getLocalHost().let { setOf(it.hostName, it.hostAddress) }
+        localHostNames.forEach {
+            localHostNameCache.put(it, true)
         }
         InetAddress.getLocalHost().run {
-            localhostNameCache.put(hostName, true)
-            localhostNameCache.put(hostAddress, true)
+            localHostNameCache.put(hostName, true)
+            localHostNameCache.put(hostAddress, true)
         }
     }
 
     private fun isLocalhost(host: String): Boolean {
-        var isLocal = localhostNameCache.get(host)
-        if (isLocal == null && resolve) {
-            isLocal = InetAddress.getAllByName(host)
-                    .map { it.hostAddress }
-                    .any { localhostNameCache.get(it) ?: false }
-            localhostNameCache.put(host, isLocal)
+        return localHostNames.contains(host) || (resolve && resolveAndCache(host))
+    }
+
+    private fun resolveAndCache(host: String): Boolean {
+        var isLocal = localHostNameCache.get(host)
+        if (isLocal == null) {
+            try {
+                isLocal = InetAddress.getAllByName(host)
+                        .map { it.hostAddress }
+                        .any { localHostNameCache.get(it) ?: false }
+            } catch (e: UnknownHostException) {
+                isLocal = false
+            }
+            localHostNameCache.put(host, isLocal)
         }
         return isLocal;
     }
